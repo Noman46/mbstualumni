6096a96101ac8c5b1539a8a028702336a74d3847
Noman46 <nomanibrahim7@gmail.com>
2019-10-06 01:25:18 +0600

    I have send mail succesfully

---
+++ mbstualumni/src/main/java/com/noman/mbstualumni/controller/EmailController.java
@@ -0,0 +1,41 @@
+package com.noman.mbstualumni.controller;
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class EmailController {
+
+	static String sendMailTo = "nomanibrahim7@gmail.com";
+	static String emailSubject = "Test mail";
+	static String emailMessage = "Success";
+	
+	@Autowired
+    public JavaMailSender mailSender;
+	
+	 @RequestMapping(value = { "/sendmail" })
+	    public String doSendEmail() {
+	       
+	         
+	       
+	        // creates a simple e-mail object
+	        SimpleMailMessage email = new SimpleMailMessage();
+	        email.setTo(sendMailTo);
+	        email.setSubject(emailSubject);
+	        email.setText(emailMessage);
+	         
+	        // sends the e-mail
+	        mailSender.send(email);
+	         
+	        // forwards to the view named "Result"
+	        return "Result";
+	    }
+  
+}

b619c0cbf877d1071193f34e9d1a700d1078a62f
Noman46 <nomanibrahim7@gmail.com>
2020-04-21 23:40:38 +0600

    Done

--- mbstualumni/src/main/java/com/noman/mbstualumni/controller/LoginPageController.java
+++ mbstualumni/src/main/java/com/noman/mbstualumni/controller/LoginPageController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.SessionScope;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.noman.alumnibackend.dao.VerifiedStudentDao;
@@ -17,36 +18,42 @@ public class LoginPageController {
 
 	@Autowired
 	public VerifiedStudentDao verifiedStudentDao;
-	
-	@RequestMapping(value = {"/loginmbstu"})
+
+	@RequestMapping(value = { "/loginmbstu" })
 	public ModelAndView giveLoginPage() {
-		
+
 		ModelAndView mv = new ModelAndView("loginmbstu");
 		mv.addObject("verifiedStudent", new VerifiedStudent());
 		return mv;
-		
+
 	}
-	
-	@RequestMapping(value = {"/student/loginToProfile"})
-	public ModelAndView logintoTheSystem(@ModelAttribute("VerifiedStudent") VerifiedStudent verifiedStudent,HttpSession session) {
-		
+
+	@RequestMapping(value = { "/student/loginToProfile" })
+	public String logintoTheSystem(@ModelAttribute("VerifiedStudent") VerifiedStudent verifiedStudent,
+			HttpSession session) {
+
 		VerifiedStudent getStudentFromDb = verifiedStudentDao.loginToSystemAsStudent(verifiedStudent);
-		if(getStudentFromDb != null) {
-			addVerifiedStudentWithSession(verifiedStudent, session);
-			ModelAndView studentProfilePage = new ModelAndView("studentProfilePage");
-			return studentProfilePage;
-			
+		if (getStudentFromDb != null) {
+			// addVerifiedStudentWithSession(verifiedStudent, session);
+			// ModelAndView studentProfilePage = new ModelAndView("studentProfilePage");
+			// return studentProfilePage;
+			return "redirect:/student/studentProfilePage?" + "applicationId=" + getStudentFromDb.getApplicationId()
+					+ "&verifiedStudentVersityId=" + getStudentFromDb.getVerifiedStudentVersityId();
+
+		} else {
+
+			return "success";
 		}
-		else {
-			ModelAndView success = new ModelAndView("success");
-			return success;
-		}	
 	}
-	
-	private void addVerifiedStudentWithSession(VerifiedStudent verifiedStudent,HttpSession session){
-		
-		session.setAttribute("applicationId", verifiedStudent.getApplicationId());
-		session.setAttribute("verifiedStudentVersityId", verifiedStudent.getVerifiedStudentId());
-			
-	}
+
+	/*
+	 * private void addVerifiedStudentWithSession(VerifiedStudent
+	 * verifiedStudent,HttpSession session){
+	 * 
+	 * session.setAttribute("applicationId", verifiedStudent.getApplicationId());
+	 * session.setAttribute("verifiedStudentVersityId",
+	 * verifiedStudent.getVerifiedStudentId());
+	 * 
+	 * }
+	 */
 }
--- mbstualumni/src/main/java/com/noman/mbstualumni/controller/StudentProfilePageController.java
+++ mbstualumni/src/main/java/com/noman/mbstualumni/controller/StudentProfilePageController.java
@@ -2,13 +2,17 @@ package com.noman.mbstualumni.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentProfilePageController {
 
 	@RequestMapping(value = {"/student/studentProfilePage"})
-	public ModelAndView giveStudentProfilePage() {
+	public ModelAndView giveStudentProfilePage(@RequestParam("applicationId")Integer applicationId,
+			                                   @RequestParam("verifiedStudentVersityId")String verifiedStudentVersityId){
+		System.out.println(applicationId);
+		System.out.println(verifiedStudentVersityId);
 		ModelAndView studentProfilePage = new ModelAndView("studentProfilePage");
 		return studentProfilePage;
 		
